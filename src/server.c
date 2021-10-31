3dc4d2597992528a10fd3b527abd5e742e637af0
jorgeedualves <jorgeedualves84@gmail.com>
2021-10-30 21:57:24 -0300

    Function in server ok: save_action

--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: joeduard <joeduard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 22:39:35 by joeduard          #+#    #+#             */
-/*   Updated: 2021/10/30 21:51:48 by joeduard         ###   ########.fr       */
+/*   Updated: 2021/10/30 21:56:29 by joeduard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,26 @@ static void sig_handler(int signal, siginfo_t *siginfo, void *context)
 	if (signal == SIGUSR2)
 		write(1, "0", 1);
 	kill(siginfo->si_pid, SIGUSR1);
-	(void)signal;
 	(void)context;
 }
-int	main (void)
 
+int save_action(void)
 {
 	struct sigaction action;
-	printf("PID: %d\n",getpid());
 	bzero(&action, sizeof(struct sigaction));
 	sigemptyset(&action.sa_mask);
 	action.sa_sigaction = sig_handler;
 	action.sa_flags = SA_SIGINFO;
 	if (sigaction(SIGUSR1, &action, NULL))
 		exit(EXIT_FAILURE);
+	if (sigaction(SIGUSR2, &action, NULL))
+		exit(EXIT_FAILURE);
+	return (0);
+}
+int	main (void)
+
+{
+	printf("PID Server: %d\n",getpid());	
 	while(1)
 		pause;
 	return (0);
