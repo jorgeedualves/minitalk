3c7164552dc28dcf0113a333d291e11b8406157e
jorgeedualves <jorgeedualves84@gmail.com>
2021-10-30 21:52:43 -0300

    function in server.c ok: sig_handler

--- src/server.c
+++ src/server.c
@@ -6,15 +6,21 @@
 /*   By: joeduard <joeduard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 22:39:35 by joeduard          #+#    #+#             */
-/*   Updated: 2021/10/30 21:44:51 by joeduard         ###   ########.fr       */
+/*   Updated: 2021/10/30 21:51:48 by joeduard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "minitalk.h"
 
+static int count;
+
 static void sig_handler(int signal, siginfo_t *siginfo, void *context)
 {
 	printf("sinal recebido %d\n", siginfo->si_pid);
+	if (signal == SIGUSR1)
+		write(1, "1", 1);
+	if (signal == SIGUSR2)
+		write(1, "0", 1);
 	kill(siginfo->si_pid, SIGUSR1);
 	(void)signal;
 	(void)context;
