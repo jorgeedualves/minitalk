954588ec0d11a2741758e3e5174cf5985472b777
jorgeedualves <jorgeedualves84@gmail.com>
2021-10-31 00:45:47 -0300

    Norminette ok minitalk.h

--- src/minitalk.h
+++ src/minitalk.h
@@ -1,7 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joeduard <joeduard@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/31 00:43:15 by joeduard          #+#    #+#             */
+/*   Updated: 2021/10/31 00:43:49 by joeduard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINITALK_H
 # define MINITALK_H
 # define _POSIX_SOURCE
-
 
 # include <unistd.h>
 # include <stdio.h>
@@ -10,4 +21,4 @@
 # include "../libraries/libft/libft.h"
 //# include "../libraries/ft_printf/ft_printf.h"
 
-# endif
+#endif
