c3187541ff0e673fc1485ccede0374d0ed3e07da
jorgeedualves <jorgeedualves84@gmail.com>
2021-10-30 21:48:33 -0300

    functions ok: wit_server_response, send_signal, process_str

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: joeduard <joeduard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 22:39:29 by joeduard          #+#    #+#             */
-/*   Updated: 2021/10/29 16:29:51 by joeduard         ###   ########.fr       */
+/*   Updated: 2021/10/30 21:44:54 by joeduard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,38 @@
 
 static int g_done;
 
-static void send_bit(int pid, const char *str, const char *client)
+void	wait_server_response(void)
+{
+	while (g_done == 0)
+		;
+	g_done = 0;
+}
+
+void	send_signal(int bit, int pid, const char *str)
+{
+
+	int count;
+	
+	count = 1 << 7;
+	while (count)
+	{
+		bit = *str & count;
+		if (bit)
+		{
+			if (kill(pid, SIGUSR1))
+				exit(EXIT_FAILURE);
+		}
+		else
+		{
+			if (kill(pid, SIGUSR2))
+			exit(EXIT_FAILURE);
+		}
+		wait_server_response();
+		count >>=1;
+	}
+}
+
+void	process_str(int pid, const char *str, const char *client)
 {
 	printf("Sinal enviado\n");
 	kill(pid, SIGUSR1);
@@ -24,13 +55,13 @@ static void send_bit(int pid, const char *str, const char *client)
 	(void)str;
 }
 
-static void get_bit(const char *str)
+void get_bit(const char *str)
 {
 	(void)str;
 
 }
 
-static void sig_handler(int signum)
+void sig_handler(int signum)
 {
 	g_done = 1;
 	(void)signal;
