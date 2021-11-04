b1db761b9ae00c4b2ef967683525b4ea70cd7fbe
jorgeedualves <jorgeedualves84@gmail.com>
2021-11-04 01:18:45 -0300

    array_bits printando certo

--- src/client.c
+++ src/client.c
@@ -6,11 +6,14 @@
 /*   By: joeduard <joeduard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 22:39:29 by joeduard          #+#    #+#             */
-/*   Updated: 2021/10/30 23:20:47 by joeduard         ###   ########.fr       */
+/*   Updated: 2021/11/04 01:11:53 by joeduard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
+#include<signal.h>
+#include<stdlib.h>
+#include<strings.h>
+#include<stdio.h>
 
 static int	g_done;
 
@@ -21,13 +24,14 @@ void	wait_server_response(void)
 	g_done = 0;
 }
 
-void	send_signal(int bit, int pid, const char *str)
+void	send_signal(int pid, const char *str)
 {
 	int	count;
+	int	bit;
 
 	count = 1 << 7;
 	while (count)
-	{
+	{						
 		bit = *str & count;
 		if (bit)
 		{
@@ -44,15 +48,15 @@ void	send_signal(int bit, int pid, const char *str)
 	}
 }
 
-void	process_str(int pid, const char *str, const char *client)
+void	process_str(int pid, const char *str)
 {
-	int	bit;
 	int	count;
-	int	index;
-
-	index = 0;
-	while (str[index++])
-		send_signal(bit, pid, str);
+	       
+	while (*str)
+	{
+		send_signal(pid, str);
+		str++;
+	}		
 	count = 8;
 	while (count--)
 	{
@@ -68,19 +72,19 @@ void	sig_handler(int signal)
 	(void)signal;
 }
 
-int	main(int argc, const char *argv[])
+int	main(int argc, char const *argv[])
 {
 	struct sigaction	action;
 	int					pid;
 
 	if (argc != 3)
 		exit(EXIT_FAILURE);
-	bzero (&action, sizeof(struct sigaction));
+	bzero(&action, sizeof(struct sigaction));
 	action.sa_handler = sig_handler;
 	if (sigaction(SIGUSR1, &action, NULL))
 		exit(EXIT_FAILURE);
 	pid = atoi(argv[1]);
-	process_str(pid, argv[2], argv[0]);
-	printf("PID Client: %d\n", getpid());
+	process_str(pid, argv[2]);
 	return (EXIT_SUCCESS);
 }
+
--- src/server.c
+++ src/server.c
@@ -6,42 +6,67 @@
 /*   By: joeduard <joeduard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 22:39:35 by joeduard          #+#    #+#             */
-/*   Updated: 2021/10/30 23:43:31 by joeduard         ###   ########.fr       */
+/*   Updated: 2021/11/04 01:09:44 by joeduard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
+#include<signal.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<strings.h>
+#include<unistd.h>
 
-static void	sig_handler(int signal, siginfo_t *siginfo, void *context)
+void convert_str(char **arr_bits)
 {
-	if (signal == SIGUSR1)
-		write(1, "1", 1);
-	if (signal == SIGUSR2)
-		write(1, "0", 1);
-	kill(siginfo->si_pid, SIGUSR1);
+     printf("Array_bits: %s\n", *arr_bits);
+}
+
+void sig_handler(int signal, siginfo_t *siginfo, void *context)
+{
+    int bit;
+    static char *arr_bits;
+    static int index;
+
+    if (arr_bits == NULL)
+        arr_bits = calloc(1, sizeof(char));
+    bit = signal == SIGUSR1;
+    if (index <= 8)
+    {
+        if (bit)
+            arr_bits[index] = '1';
+        else
+            arr_bits[index] = '0';
+        index++;
+    }
+    if(index == 8)
+    {
+        convert_str(&arr_bits);
+        free(arr_bits);
+        arr_bits = NULL;
+        index = 0;
+    }
+    kill(siginfo->si_pid, SIGUSR1);
 	(void)context;
 }
 
-int	save_actions(void)
-{
-	struct sigaction	action;
-
-	bzero (&action, sizeof(struct sigaction));
-	sigemptyset(&action.sa_mask);
-	action.sa_sigaction = sig_handler;
-	action.sa_flags = SA_SIGINFO;
-	if (sigaction(SIGUSR1, &action, NULL))
-		exit(EXIT_FAILURE);
-	if (sigaction(SIGUSR2, &action, NULL))
-		exit(EXIT_FAILURE);
-	return (0);
+int save_actions(void){
+    struct sigaction action;
+    bzero(&action, sizeof(struct sigaction));
+    sigemptyset(&action.sa_mask);
+    action.sa_sigaction = sig_handler;
+    action.sa_flags = SA_SIGINFO;
+    if (sigaction(SIGUSR1, &action, NULL))
+        exit(EXIT_FAILURE);
+    if (sigaction(SIGUSR2, &action, NULL))
+		    exit(EXIT_FAILURE);
+    return (0);
 }
-
-int	main(void)
+    
+int main(void)
 {
-	printf ("PID Server: %d\n", getpid());
-	save_actions();
-	while (1)
-		pause();
-	return (0);
+    printf("Server running... [PID]: %d\n", getpid());
+    save_actions();
+    while (1)
+        pause();
+    return (0);
 }
