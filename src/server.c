9b28bc00aaf421724088ba5ac6ba8ff526c02bc2
jorgeedualves <jorgeedualves84@gmail.com>
2021-11-09 20:46:04 -0300

    modifield: main

--- src/server.c
+++ src/server.c
@@ -6,48 +6,17 @@
 /*   By: joeduard <joeduard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 22:39:35 by joeduard          #+#    #+#             */
-/*   Updated: 2021/11/09 20:24:27 by joeduard         ###   ########.fr       */
+/*   Updated: 2021/11/09 20:45:18 by joeduard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include "libft.h"
 
-char	*put_first_char(char current)
+char	*print_str(char *str)
 {
-	char	*add;
-	int		i;
-
-	i = 0;
-	add = (char *)malloc(sizeof(char) + 1);
-	if (!add)
-		return (NULL);
-	add[i++] = current;
-	add[i] = '\0';
-	return (add);
-}
-
-char	*ft_add_current(char *str, char current)
-{
-	char	*add;
-	int		i;
-
-	i = 0;
-	if (!str)
-		return (put_first_char(current));
-	if (!current)
-		return (NULL);
-	add = (char *)malloc(sizeof(char) * (strlen(str) + 2));
-	if (!add)
-		return (NULL);
-	while (str[i])
-	{
-		add[i] = str[i];
-		i++;
-	}
-	add[i++] = current;
-	add[i++] = '\0';
-	return (add);
+	ft_putstr_fd(str, 1);
+	return (NULL);
 }
 
 void	sig_handler(int signal, siginfo_t *siginfo, void *context)
@@ -65,7 +34,7 @@ void	sig_handler(int signal, siginfo_t *siginfo, void *context)
 	if (++bits == 8)
 	{
 		if (current)
-			message = ft_add_current(message, current);
+			message = ft_strappendchar(message, current);
 		else
 		{
 			message = print_str(message);
@@ -83,7 +52,7 @@ int	save_actions(void)
 {
 	struct sigaction	action;
 
-	bzero (&action, sizeof(struct sigaction));
+	ft_bzero (&action, sizeof(struct sigaction));
 	sigemptyset(&action.sa_mask);
 	action.sa_sigaction = sig_handler;
 	action.sa_flags = SA_SIGINFO;
@@ -96,7 +65,12 @@ int	save_actions(void)
 
 int	main(void)
 {
-	printf("Server running... [PID]: %d\n", getpid());
+	char	*pid;
+
+	pid = ft_itoa(getpid());
+	ft_putstr_fd("Server running... [PID]: ", 1);
+	ft_putstr_fd(pid, 1);
+	ft_putstr_fd("\n", 1);
 	save_actions();
 	while (1)
 		pause();
